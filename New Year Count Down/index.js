23b2d9cd7149b56d1af9d74504a637c775d2c112
Eric-Obeng <obengeric.3958@gmail.com>
2023-04-12 00:12:24 +0000

    Final codes

--- New Year Count Down/index.js
+++ New Year Count Down/index.js
@@ -2,13 +2,32 @@ const dayDiv = document.getElementById("day")
 const hourDiv = document.getElementById("hour")
 const minuteDiv = document.getElementById("minutes")
 const secondDiv = document.getElementById("seconds")
-const millisecondDiv = document.getElementById("milliseconds")
 
-const newYearDate = new Date("Jan 1, 2024 00:00:00:00").getTime()
+
+const newYearTime = new Date("Jan 1, 2024 00:00:00:00").getTime()
 
 updateCountdown()
 
 function updateCountdown() {
     const now = new Date().getTime()
-    console.log(now)
+    const gap = newYearTime - now
+    
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const d = Math.floor(gap / day)
+    const h = Math.floor((gap % day) / hour)
+    const m = Math.floor((gap % hour) / minute)
+    const s = Math.floor((gap % minute) / second)
+    
+
+    dayDiv.innerText = d;
+    hourDiv.innerText = h
+    minuteDiv.innerText = m
+    secondDiv.innerText = s
+    
+
+    setTimeout(updateCountdown, 1000)
 }
