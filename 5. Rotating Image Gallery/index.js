ff8410112f36ee0eab0fecf742ff510eccfc39f4
Eric-Obeng <obengeric.3958@gmail.com>
2023-07-23 22:58:22 +0000

    Rotating image gallery

--- 5. Rotating Image Gallery/index.js
+++ 5. Rotating Image Gallery/index.js
@@ -2,9 +2,11 @@ const imageContainerEl = document.querySelector(".image-container")
 const prevEl = document.getElementById("prev")
 const nextEl = document.getElementById("next") 
 
-let x = 0
+let x = 0;
+let timer;
 prevEl.addEventListener("click", ()=> {
     x += 45;
+    clearTimeout(timer)
     updateGallery()
 })
 
@@ -12,9 +14,15 @@ const updateGallery = () => {
     imageContainerEl.style.transform = `
         perspective(1000px) rotateY(${x}deg)
     `
+    timer = setTimeout(()=> {
+        x -= 45;
+        updateGallery()
+    }, 3000)
 }
+updateGallery()
 
 nextEl.addEventListener("click", ()=> {
     x -= 45;
+    clearTimeout(timer)
     updateGallery()
 })
